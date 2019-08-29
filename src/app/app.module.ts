f8684b09284cbf7d5eefdf844ab31aa595873fd0
Kasim Ridwan <kasimsheyi20@gmail.com>
2019-08-30 00:23:40 +0100

    recent changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { TaskManagerComponent } from './components/task-manager/task-manager.component';
-import { TaskFormComponent } from './components/task-manager/task-form/task-form.component';
-import { TaskListComponent } from './components/task-manager/task-list/task-list.component';
-import { TaskDetailComponent } from './components/task-manager/task-detail/task-detail.component';
-import { TaskItemComponent } from './components/task-manager/task-list/task-item/task-item.component';
-import { LoginComponent } from './components/login/login.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { TaskManagerComponent } from "./components/task-manager/task-manager.component";
+import { TaskFormComponent } from "./components/task-manager/task-form/task-form.component";
+import { TaskListComponent } from "./components/task-manager/task-list/task-list.component";
+import { TaskDetailComponent } from "./components/task-manager/task-detail/task-detail.component";
+import { TaskItemComponent } from "./components/task-manager/task-list/task-item/task-item.component";
+import { LoginComponent } from "./components/login/login.component";
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
+import { NgSelectModule } from "@ng-select/ng-select";
 
 @NgModule({
   declarations: [
@@ -30,9 +31,10 @@ import { HttpClientModule } from "@angular/common/http";
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    NgSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
